3a0de4ced1e547e3e9c2e6aaeb51eba20356a9ef
midxdle <sina.heydarian.sh@gmail.com>
2021-08-12 11:09:30 +0430

    due to last commit

--- express.js
+++ express.js
@@ -1,11 +1,11 @@
-//npm install express
-
 const express = require('express');
 
 let app = express();
 
 app.set('view engine', 'jade');
 
-app.get('/', function(req, res) {});
+app.get('/', function(req, res) {
+    res.send('Hi to the world out there!')
+});
 
 let server = app.listen(8080, ()=>{});
