b76506ec45b1c6a8fca82fe3ede1e98dd8a4e451
midxdle <sina.heydarian.sh@gmail.com>
2021-08-12 16:02:07 +0430

    pug file added and some new lines writed in express to use pug

--- express.js
+++ express.js
@@ -2,10 +2,24 @@ const express = require('express');
 
 let app = express();
 
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.get('/', function(req, res) {
-    res.send('Hi to the world out there!')
+    res.render('index', 
+    {title:'midxdle', message:'I am a programmer'})
+   //res.send('Hi to the world out there!')
 });
 
-let server = app.listen(8080, ()=>{});
+let server = app.listen(3000, ()=>{});
+
+app.route('/node').get(function(req, res) {
+    res.send("node is so exciting");
+});
+
+app.route('/javascript').get(function(req, res) {
+    res.send('javascript is so powerfull');
+});
+
+app.get('/', function(req, res) {
+    res.send("i really enjoy programming");
+});
