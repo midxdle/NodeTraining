b7d543a6528a53fc18befbe619e970cd5e72afa6
midxdle <sina.heydarian.sh@gmail.com>
2021-08-13 17:15:03 +0430

    working with event method and event.js updated, due to changes json updated too

--- event.js
+++ event.js
@@ -7,6 +7,7 @@ rs.on('open', function() {
 
 //EventEmitter
 var events = require('events');
+const { emit } = require('process');
 var emitter = new events.EventEmitter();
 
 //event handler
@@ -19,3 +20,22 @@ emitter.on('handler', handler);
 
 //fire the event
 emitter.emit('handler');
+
+//once method
+emitter.once('handler', handler);
+emitter.emit('handler'); //event will be triggered
+emitter.emit('handler'); //event won't be triggered
+
+//ListenerCount
+emitter.on('handler', handler);
+emitter.on('handler', handler);
+
+console.log(events.EventEmitter(emitter, 'handler'));
+
+//NewListener
+emitter.on('NewListener', function(eventName, listener) {
+    console.log('added listener for ' + eventName + 'events');
+});
+
+emitter.on('handler', handler);
+emitter.on('handler', handler);
